aa69c78369f2384499c4f7df42315516f08af2ac
Behruz Mansurov <bekha.m2000.bm@gmail.com>
2021-04-11 01:36:07 +0300

    swagger enable

--- TaskJava/phoneBook/src/main/java/com/example/phoneBook/PhoneBookApplication.java
+++ TaskJava/phoneBook/src/main/java/com/example/phoneBook/PhoneBookApplication.java
@@ -2,8 +2,10 @@ package com.example.phoneBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PhoneBookApplication {
 
 	public static void main(String[] args) {
